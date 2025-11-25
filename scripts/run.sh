5df73a760f4b5bd2131eccbfbcc1dcf4a08a7220
Bobby <30593201+luciferreeves@users.noreply.github.com>
2025-11-25 18:59:54 +0530

    update run script for new image format

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 qemu-system-x86_64 \
     -drive if=pflash,format=raw,readonly=on,file=/opt/homebrew/share/qemu/edk2-x86_64-code.fd \
-    -drive format=raw,file=iso/akiba.iso \
+    -drive format=raw,file=iso/akiba.img \
     -m 2048M \
     -serial stdio
