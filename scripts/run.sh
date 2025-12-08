72ebf27948b995ec4b4a0ac45ff2527b45d35f4a
Bobby <30593201+luciferreeves@users.noreply.github.com>
2025-12-08 19:54:44 +0530

    update run for linux and mac

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+UEFI_FW="/opt/homebrew/share/qemu/edk2-x86_64-code.fd"
+if [ ! -f "$UEFI_FW" ]; then
+    UEFI_FW="/usr/share/edk2/x64/OVMF_CODE.fd"
+fi
+
 qemu-system-x86_64 \
     -M q35 \
-    -drive if=pflash,format=raw,readonly=on,file=/opt/homebrew/share/qemu/edk2-x86_64-code.fd \
-    -drive file=iso/akiba.img,format=raw,if=none,id=maindisk \
-    -device ide-hd,drive=maindisk,bus=ide.2,bootindex=0 \
+    -drive if=pflash,format=raw,readonly=on,file="$UEFI_FW" \
+    -drive file=iso/akiba.img,format=raw \
     -m 2048M \
     -serial stdio
